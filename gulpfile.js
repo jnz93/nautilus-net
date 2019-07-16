85450e05b279491df5931ffe7e44301f0f6af8cb
jz93 <box@unitycode.tech>
2019-07-16 17:04:18 -0300

    Adição da style guide inicial, botões e configurações de plugins gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var cleanDir = require('gulp-clean-dir');
 
 gulp.task('compileSass', function(){
     return gulp
-        .src('css/sass/*.sass') // path of archives to compile
+        .src('css/sass/parts/*.sass') // path of archives to compile
         // .pipe(cleanDir('css', {ext: '.css'})) Não está funcionando
         .pipe(sass())
-        .pipe(gulp.dest('css')) // path of to compile archives
+        .pipe(gulp.dest('css/parts')) // path of to compile archives
 })
