3647a2ec4f5e600c0f67d78f64fb40f50733ad0f
jz93 <box@unitycode.tech>
2019-07-22 15:22:08 -0300

    add footer inicial

--- footer.php
+++ footer.php
@@ -1,2 +1,20 @@
-</body>
+<?php 
+/**
+ * The footer for our theme
+ */
+?>
+    <footer class="container-fluid footer-t1">
+        <?php 
+        if( shortcode_exists('get_footer_type_1')){
+            do_shortcode('[get_footer_type_1]');
+        } else {
+            echo "Shortcode inexistente";
+        }
+        ?>
+        <script>
+            eva.replace();
+        </script>
+    </footer>
+    <?php wp_footer(); ?>
+    </body>
 </html>
