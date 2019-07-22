745f47e1e227e455f6630e5a96c3cf90e7993bb8
jz93 <box@unitycode.tech>
2019-07-22 16:10:30 -0300

    Add arquitetura seção tpl-home

---
+++ templates/tpl-home.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * The template for displaying sliders and notifications on homepage
+ * 
+ * @package nautilusnet
+ */
+?>
+<section id="" class="">
+    <?php 
+    if( shortcode_exists('show_notifications') ){
+        do_shortcode('[show_notifications]');
+    }else{
+        echo "Shortcode de notificações não existe.";
+    }
+
+    if( shortcode_exists('show_article_sliders') ){
+        do_shortcode('[show_article_sliders]');
+    }else{
+        echo "Shortcode de sliders não existe";
+    }
+    ?>
+</section>
