3a526a1d4fcbd273aee4caed5a5d3ea6df69b6e2
jz93 <box@unitycode.tech>
2019-08-22 14:36:53 -0300

    Fix id google analytics

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -352,7 +352,7 @@ add_filter('login_headertitle', 'company_title_logo');
  */
 function snippet_ganalytics(){
     $g_analytics_id = get_option('google_analitycs');
-    $snippet_js = '<script async src="https://www.googletagmanager.com/gtag/js?id='. $g_analytics_id .'"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", '. $g_analytics_id .');</script>';
+    $snippet_js = '<script async src="https://www.googletagmanager.com/gtag/js?id='. $g_analytics_id .'"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "'. $g_analytics_id .'");</script>';
 
     echo $snippet_js;
 }
