6abda54dd1b0cbe59ba4515ebe4ee9d41a8bd5f1
jz93 <box@unitycode.tech>
2019-09-26 15:19:15 -0300

    Fix get homepage custom

--- index.php
+++ index.php
@@ -4,21 +4,5 @@
  * @package nautilusnet
  */
 get_header();
-?>
-<div id="" class="body-content">
-    <?php 
-    if( have_posts() && false ){
-        while( have_posts() ){
-            the_post(); ?>
-            <div class="">
-                <?php get_template_part('content/content', get_post_format() ); ?>
-            </div>
-        <?php
-        }
-    } else {
-        get_template_part('homepage-custom');
-    }
-    ?>
-</div>
-
-<?php get_footer(); ?>
+get_template_part('homepage-custom');
+get_footer();
