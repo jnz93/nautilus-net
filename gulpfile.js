22662b48ecec942256f1496c5bfc7a286c0ea108
jz93 <box@unitycode.tech>
2019-09-30 11:43:03 -0300

    Config para compilar sass fora da pasta /parts

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 var pathSass = 'css/sass/parts/*.sass';
+var pathSass2 = 'css/sass/*.sass';
 gulp.task('sass', function(){
     return gulp
         .src(pathSass)
@@ -11,4 +12,5 @@ gulp.task('sass', function(){
 
 gulp.task('watchCompile', function(){
     gulp.watch(pathSass, gulp.series('sass'));
+    // gulp.watch(pathSass2, gulp.series('sass'));
 });
