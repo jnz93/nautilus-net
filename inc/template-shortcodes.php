b58ac43fc9d0aa8915871fc28924336aab075197
jz93 <box@unitycode.tech>
2019-10-01 17:02:29 -0300

    Alterar classe e texto do botão do post

--- inc/template-shortcodes.php
+++ inc/template-shortcodes.php
@@ -166,7 +166,7 @@ if( !function_exists('get_article_sliders') ){
                                 <div class="col-lg-6 articleHome__content--desktop">
                                     <h1 class="articleHome__title"><?php echo $post_title ?></h1>
                                     <p class="articleHome__excerpt"><?php echo $post_excerpt ?></p>
-                                    <a href="<?php echo $post_link ?>" class="btn btn__primary btn__primary--normal">Saiba mais</a>
+                                    <a href="<?php echo $post_link ?>" class="btn btn__primary btn__primary--big">Ler publicação</a>
                                 </div> <!-- /End title, excerpt and button container desktop-->
                                 <div class="col-xs-10 col-sm-10 articleHome__content--mobile">
                                     <a href="<?php echo $post_link ?>">
